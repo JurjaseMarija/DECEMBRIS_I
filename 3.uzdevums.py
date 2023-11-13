cb604acacb5b59a039e0ee988725f1e51026b8d0
Marija Jurjase <jurjasemarija30@gmail.com>
2023-11-13 09:53:33 +0200

    3.uzdevums

--- 3.uzdevums.py
+++ 3.uzdevums.py
@@ -1,1 +1,11 @@
-skaitlis = int(input("Ievadiet skaitli: "))
+number = (input("number: "))
+if number == 0:
+    print("skaitlis ir 0")
+elif number < 0:
+    print("skaitlis ir negatīvs")
+elif number % 2 == 0:
+    print(number, " ir pāru")
+elif number % 2 != 0:
+    print(number, " ir nepāru")
+else:
+    print("Ievadiet skaitli")
